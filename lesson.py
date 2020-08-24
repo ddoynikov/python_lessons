b3498be722ffec998f472491143a41169191b875
Denis Doynikov <denis.doynikov@gmail.com>
2020-08-24 16:50:10 +0300

    24.08

--- lesson.py
+++ lesson.py
@@ -1,7 +1,13 @@
-a = 'Правильность'
+a = int(input())
+if a == 1:
+    print(0)
+elif a % 2 != 0:
+    print('НЕТ')
+else:
+    step = 0
+    while a != 1:
+        a = a / 2
+        step+=1
+    print(step)
 
-    
-while len(a) > 0:
-    print(a) 
-    a=a[1:-1] 
 
